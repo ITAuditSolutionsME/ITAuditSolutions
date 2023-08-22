26877a68fe1377f396e295aef3b1305e13a97273
ITAuditSolutionsME <142792689+ITAuditSolutionsME@users.noreply.github.com>
2023-08-22 09:16:56 +0200

    Add files via upload

---
+++ Zeilen_Trennen.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import javax.swing.JOptionPane;
+
+public class Zeilen_Trennen {
+    public static void main(String[] args) {
+        String sourceFolderPath = JOptionPane.showInputDialog("Eingabe Pfad?"); // Pfad zum Quellordner anpassen
+        String destinationFolderPath = JOptionPane.showInputDialog("Ausgabe Pfad?");  // Pfad zum Zielordner anpassen
+
+        File sourceFolder = new File(sourceFolderPath);
+        File[] sourceFiles = sourceFolder.listFiles();
+
+        for (File sourceFile : sourceFiles) {
+            if (sourceFile.isFile()) {
+                int id = 1; // Zurücksetzen der ID für jede neue Quelldatei
+                //Die varible id beginnt bei 1 und jedes mal wenn eine Zeile mit F kommt wird dieser um 1 erhöht.
+                //Alle anderen Zeilen zwischen zwei F`s bekommen die ID, des letzten verherigen F`s
+
+                try {
+                    BufferedReader reader = new BufferedReader(new FileReader(sourceFile));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        char firstChar = line.charAt(0);
+                        //Hier wird geschaut ob die Zeile mit einem F beginnt
+                        if (firstChar == 'F') {
+                            writeToFile(destinationFolderPath, sourceFile.getName(), "F", "ID:" + id + ";" + line);
+                            ++id;
+                            //Hier wird geschaut ob die Zeile mit K beginnt
+                        } else if (firstChar == 'K') {
+                            writeToFile(destinationFolderPath, sourceFile.getName(), "K", "ID:" + (id - 1) + ";" + line);
+                            //Hier wird geschaut ob die Zeile mit G beginnt und Ust. oder Vst. in der Zeile vorkommt
+                        } else if (firstChar == 'G' && (line.contains("Ust.") || line.contains("Vst."))) {
+                            writeToFile(destinationFolderPath, sourceFile.getName(), "Steuerart", "ID:" + (id - 1) + ";" + line);
+                            //All anderen Zeilen, also in diesem Fall alle welche mit G beginnen aber keine Ust. oder Vst. haben
+                        } else {
+                            writeToFile(destinationFolderPath, sourceFile.getName(), "G", "ID:" + (id - 1) + ";" + "G2;" + line);
+                        }
+                    }
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+//Hier werden die Zeilen in die jeweiligen neu erzeugten Datein geschrieben und richtig benannt.
+//Benannt werden die Datein wie folgt: Name der Quelldatei + ein Merkmal der Zeilen welche sie abbilden
+    private static void writeToFile(String destinationFolderPath, String sourceFileName, String marker, String content) {
+        String newFileName = sourceFileName + "_" + marker + ".txt";
+        File newFile = new File(destinationFolderPath, newFileName);
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(newFile, true));
+            writer.write(content);
+            writer.newLine();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
